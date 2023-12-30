cf0703d0aeeee6cb6149d21e8ab7fef84626ea8d
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2023-12-31 00:58:03 +0300

    Comments created

--- app/Comments.js
+++ app/Comments.js
@@ -1,63 +1,107 @@
 // Comments.js
+
 import React, { useState } from 'react';
-import { View, Text, FlatList, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Comments = () => {
-  const [selectedFacilityId, setSelectedFacilityId] = useState(null);
+  const [selectedFacility, setSelectedFacility] = useState(null);
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
+  const [userRating, setUserRating] = useState(0);
 
   const facilities = [
-    { id: '1', name: 'Facility 1' },
-    { id: '2', name: 'Facility 2' },
+    { id: '1', name: 'Futbol Sahası' },
+    { id: '2', name: 'Basketbol Sahası' },
     // ... diğer tesisler
   ];
 
-  const handleFacilitySelection = (facilityId) => {
-    setSelectedFacilityId(facilityId);
+  const handleFacilitySelection = (facility) => {
+    setSelectedFacility(facility);
     // Belirli bir tesis seçildiğinde, o tesise ait yorumları getirebilirsiniz.
-    // Örnek: fetchCommentsForFacility(facilityId);
+    // Örnek: fetchCommentsForFacility(facility.id);
   };
 
   const handleAddComment = () => {
     // Yeni yorumu eklemek için
-    if (selectedFacilityId && newComment.trim() !== '') {
-      setComments([...comments, { id: comments.length + 1, text: newComment }]);
+    if (selectedFacility && newComment.trim() !== '') {
+      setComments([
+        ...comments,
+        {
+          id: comments.length + 1,
+          text: newComment,
+          user: 'John Doe',
+          rating: userRating,
+        },
+      ]);
       setNewComment('');
+      setUserRating(0);
 
       // Yeni yorumu sunucuya göndermek için bir fonksiyonu çağırabilirsiniz.
-      // Örnek: sendCommentToServer(selectedFacilityId, newComment);
+      // Örnek: sendCommentToServer(selectedFacility.id, newComment);
     }
+  };
+
+  const renderStar = (index) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        onPress={() => setUserRating(index + 1)}
+        style={[styles.star, index < userRating ? styles.filledStar : null]}
+      />
+    );
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Yorumlar</Text>
+      <Text style={styles.title}>Facilities</Text>
 
-      <View style={styles.facilityList}>
-        <Text>Facility'yi Seç:</Text>
+      {!selectedFacility ? (
         <FlatList
           data={facilities}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <Button
-              title={item.name}
-              onPress={() => handleFacilitySelection(item.id)}
-              disabled={selectedFacilityId === item.id}
-            />
+            <TouchableOpacity
+              style={styles.facilityButton}
+              onPress={() => handleFacilitySelection(item)}
+            >
+              <Text>{item.name}</Text>
+            </TouchableOpacity>
           )}
-          horizontal
         />
-      </View>
-
-      {selectedFacilityId ? (
+      ) : (
         <View style={styles.commentsContainer}>
-          <Text>Yorumlar:</Text>
+          <Text>{selectedFacility.name} için Yorumlar:</Text>
           <FlatList
             data={comments}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <View style={styles.commentItem}>
+                <Text style={styles.commentUser}>{item.user}</Text>
+                <Text>{item.text}</Text>
+                <View style={styles.commentRating}>
+                  <View style={styles.starsContainer}>
+                    {[0, 1, 2, 3, 4].map((index) => (
+                      <View
+                        key={index}
+                        style={[
+                          styles.star,
+                          index < item.rating ? styles.filledStar : null,
+                        ]}
+                      />
+                    ))}
+                  </View>
+                  <Text style={styles.commentRatingText}>({item.rating}/5)</Text>
+                </View>
+              </View>
+            )}
           />
+
+          <View style={styles.ratingContainer}>
+            <Text>Değerlendir:</Text>
+            <View style={styles.starsContainer}>
+              {[0, 1, 2, 3, 4].map((index) => renderStar(index))}
+            </View>
+          </View>
 
           <TextInput
             style={styles.input}
@@ -67,8 +111,6 @@ const Comments = () => {
           />
           <Button title="Yorum Ekle" onPress={handleAddComment} />
         </View>
-      ) : (
-        <Text>Lütfen bir tesis seçin.</Text>
       )}
     </View>
   );
@@ -85,11 +127,37 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 16,
   },
-  facilityList: {
-    marginBottom: 16,
+  facilityButton: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 8,
+    marginVertical: 8,
   },
   commentsContainer: {
     marginTop: 16,
+  },
+  commentItem: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 8,
+    marginVertical: 8,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  commentUser: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    backgroundColor: '#ffd700', // Yorum atan kişinin adının arka planı sarı renkli
+    padding: 8,
+    marginBottom: 8,
+  },
+  commentRating: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  commentRatingText: {
+    marginLeft: 8,
   },
   input: {
     height: 40,
@@ -98,6 +166,25 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 12,
     padding: 8,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  starsContainer: {
+    flexDirection: 'row',
+    marginLeft: 8,
+  },
+  star: {
+    width: 20,
+    height: 20,
+    margin: 2,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  filledStar: {
+    backgroundColor: '#f8d64e',
   },
 });
 
