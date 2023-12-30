d9c4c37814ed5c96f5abd7f0a40f34806e3def0b
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2023-12-30 23:17:24 +0300

    reservation created

--- app/RankingPage.js
+++ app/RankingPage.js
@@ -28,14 +28,16 @@ const RankingPage = () => {
           data={users}
           keyExtractor={(item) => item.id}
           renderItem={({ item, index }) => (
-            <View style={styles.userItem}>
-              <Text>{`${index + 1}. ${item.name}`}</Text>
-              <Text>{`Rank: ${item.rank}`}</Text>
+            <View style={index % 2 === 0 ? styles.evenItem : styles.oddItem}>
+              <View style={styles.userInfoContainer}>
+                <Text style={styles.userName}>{`${index + 1}. ${item.name}`}</Text>
+                <Text style={styles.userRank}>{`  Rank: ${item.rank}`}</Text>
+              </View>
             </View>
           )}
         />
       ) : (
-        <Text>Kullanıcı bulunamadı.</Text>
+        <Text style={styles.noUserText}>Kullanıcı bulunamadı.</Text>
       )}
     </View>
   );
@@ -47,16 +49,44 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
+    backgroundColor: '#f5f5f5', // Arka plan rengi
   },
   title: {
     fontSize: 24,
     marginBottom: 16,
+    color: '#333', // Başlık rengi
   },
-  userItem: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 8,
+  evenItem: {
+    borderWidth: 3, // Kalın kenar
+    borderColor: 'red', // Kenar rengi
+    padding: 16,
     marginVertical: 8,
+    backgroundColor: '#f9f9f9', // Çift sıradaki öğelerin arka plan rengi
+  },
+  oddItem: {
+    borderWidth: 3, // Kalın kenar
+    borderColor: 'red', // Kenar rengi
+    padding: 16,
+    marginVertical: 8,
+    backgroundColor: '#fff', // Tek sıradaki öğelerin arka plan rengi
+  },
+  userInfoContainer: {
+    flexDirection: 'row', // Yatay düzen
+    justifyContent: 'space-between', // İki öğeyi genişlikleri doğrultusunda eşit aralıklarla yerleştirme
+    alignItems: 'center', // Dikeyde hizalama
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333', // Kullanıcı adı rengi
+  },
+  userRank: {
+    fontSize: 16,
+    color: '#666', // Rank rengi
+  },
+  noUserText: {
+    fontSize: 16,
+    color: '#666', // Bulunamayan kullanıcı metni rengi
   },
 });
 
--- app/Reservation.js
+++ app/Reservation.js
@@ -1,15 +1,48 @@
-// Reservation.js
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';  // Make sure to include this line
+import FacilityCard from './FacilityCard';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { TouchableOpacity } from 'react-native';
+import ShowTimeSlots from './ShowTimeSlots';
 
+LocaleConfig.locales['tr'] = {
+  monthNames: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+  monthNamesShort: ['Oca.', 'Şub.', 'Mar.', 'Nis.', 'May.', 'Haz.', 'Tem.', 'Ağu.', 'Eyl.', 'Eki.', 'Kas.', 'Ara.'],
+  dayNames: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+  dayNamesShort: ['Paz.', 'Pzt.', 'Sal.', 'Çar.', 'Per.', 'Cum.', 'Cmt.'],
+};
+
+LocaleConfig.defaultLocale = 'tr';
 const Reservation = () => {
   const [reservationName, setReservationName] = useState('');
-  const [reservationDate, setReservationDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState(null);
   const [reservationTime, setReservationTime] = useState('');
+  const [showCalendar, setShowCalendar] = useState(false); // Yeni state
+  const [showTimeSlots, setShowTimeSlots] = useState(false); // Yeni state
+
+  const facilities = [
+    { id: '1', name: 'Futbol Sahası A', type: 'Futbol' },
+    { id: '2', name: 'Basketbol Sahası B', type: 'Basketbol' },
+    // Diğer tesisleri ekleyebilirsiniz
+  ];
 
   const handleCreateReservation = () => {
-    // Burada rezervasyon oluşturma işlemlerini gerçekleştirebilirsiniz
-    console.log('Rezervasyon Oluşturuluyor: ', reservationName, reservationDate, reservationTime);
+    console.log('Rezervasyon Oluşturuluyor: ', reservationName, selectedDate, reservationTime);
+  };
+
+  const handleFacilityPress = (facility) => {
+    console.log('Tesis Seçildi: ', facility);
+    setShowCalendar(true); // Tesis kartına tıklandığında takvimi göster
+  };
+
+  const handleTimeSlotPress = (startTime, endTime) => {
+    console.log('Seçilen Zaman Aralığı: ', startTime, endTime);
+    // Burada seçilen zaman aralığına göre işlemleri gerçekleştirebilirsiniz
+  };
+
+  const handleCalendarClose = () => {
+    setShowCalendar(false); // Takvim kapatıldığında
   };
 
   return (
@@ -21,12 +54,40 @@ const Reservation = () => {
         onChangeText={(text) => setReservationName(text)}
         value={reservationName}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Rezervasyon Tarihi"
-        onChangeText={(text) => setReservationDate(text)}
-        value={reservationDate}
+      <FlatList
+        data={facilities}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <FacilityCard facility={item} onPress={() => handleFacilityPress(item)} />
+        )}
       />
+      {showCalendar && (
+        <View style={styles.calendarContainer}>
+          <Calendar
+            onDayPress={(day) => {
+              setSelectedDate(day.dateString);
+              setShowTimeSlots(true);
+            }}
+            monthFormat={'MMMM yyyy'}
+            markedDates={{ [selectedDate]: { selected: true, disableTouchEvent: true } }}
+            theme={{
+              selectedDayBackgroundColor: '#3498db',
+              todayTextColor: '#3498db',
+              arrowColor: '#3498db',
+            }}
+          />
+          <TouchableOpacity onPress={handleCalendarClose} style={styles.closeCalendarButton}>
+            <Text style={styles.closeCalendarText}>Kapat</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {showTimeSlots && (
+        <ShowTimeSlots
+          selectedDate={selectedDate}
+          onClose={() => setShowTimeSlots(false)}
+          onTimeSlotPress={handleTimeSlotPress}
+        />
+      )}
       <TextInput
         style={styles.input}
         placeholder="Rezervasyon Saati"
@@ -39,23 +100,24 @@ const Reservation = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  // ... (diğer stiller)
+
+  calendarContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
     padding: 16,
-  },
-  title: {
-    fontSize: 24,
+    marginTop: 16,
     marginBottom: 16,
   },
-  input: {
-    height: 40,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 12,
+  closeCalendarButton: {
+    backgroundColor: '#3498db',
     padding: 8,
+    borderRadius: 8,
+    marginTop: 8,
+  },
+  closeCalendarText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
