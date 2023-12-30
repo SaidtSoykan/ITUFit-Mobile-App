a45f6330e9ad7c174f578de0e94eaedf5221dc7c
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2023-12-31 00:39:23 +0300

    reservation management

--- app/ReservationManagement.js
+++ app/ReservationManagement.js
@@ -1,14 +1,24 @@
 // ReservationManagement.js
 import React, { useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 const ReservationManagement = () => {
   // Örnek rezervasyonlar
   const [reservations, setReservations] = useState([
-    { id: '1', name: 'Rezervasyon 1', date: '2023-01-01', time: '12:00' },
-    { id: '2', name: 'Rezervasyon 2', date: '2023-02-01', time: '14:30' },
+    { id: '1', name: 'Futbol Sahası', date: '2023-01-01', time: '12:00' },
+    { id: '2', name: 'Basketbol Sahası', date: '2023-02-01', time: '14:30' },
     // ... diğer rezervasyonlar
   ]);
+
+  const [selectedReservation, setSelectedReservation] = useState(null);
+
+  const handleCancelReservation = () => {
+    if (selectedReservation) {
+      const updatedReservations = reservations.filter((item) => item.id !== selectedReservation.id);
+      setReservations(updatedReservations);
+      setSelectedReservation(null);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -18,15 +28,27 @@ const ReservationManagement = () => {
           data={reservations}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <View style={styles.reservationItem}>
-              <Text>{item.name}</Text>
-              <Text>Tarih: {item.date}</Text>
-              <Text>Saat: {item.time}</Text>
-            </View>
+            <TouchableOpacity
+              onPress={() => setSelectedReservation(item)}
+              style={[
+                styles.reservationItem,
+                selectedReservation && selectedReservation.id === item.id && styles.selectedItem,
+              ]}
+            >
+              <Text style={styles.reservationText}>{item.name}</Text>
+              <Text style={styles.reservationText}>Tarih: {item.date}</Text>
+              <Text style={styles.reservationText}>Saat: {item.time}</Text>
+            </TouchableOpacity>
           )}
         />
       ) : (
         <Text>Henüz bir rezervasyon yapılmamış.</Text>
+      )}
+
+      {selectedReservation && (
+        <TouchableOpacity onPress={handleCancelReservation} style={styles.cancelButton}>
+          <Text style={styles.cancelButtonText}>İptal Et</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
@@ -44,10 +66,28 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   reservationItem: {
-    borderWidth: 1,
-    borderColor: 'gray',
+    borderWidth: 2,
+    borderColor: 'green',
+    borderRadius: 8,
     padding: 8,
     marginVertical: 8,
+  },
+  selectedItem: {
+    backgroundColor: 'lightgreen',
+  },
+  reservationText: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+    padding: 8,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  cancelButtonText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
 
