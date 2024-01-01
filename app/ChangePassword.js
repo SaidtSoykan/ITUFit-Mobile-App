8576b754262b2111a8259a88c1f04e07317aaf36
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2024-01-01 23:55:00 +0300

    changepassword done

--- app/ChangePassword.js
+++ app/ChangePassword.js
@@ -1,11 +1,48 @@
-// ChangePassword.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
-const ChangePassword = () => {
+const ChangePasswordScreen = () => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+  const handleChangePassword = () => {
+    // Örnek kontroller
+    if (currentPassword === 'gecerli_sifre' && newPassword === confirmNewPassword) {
+      // Şifre değiştirme işlemleri burada gerçekleştirilebilir.
+      Alert.alert('Başarılı', 'Şifre değiştirildi!');
+    } else {
+      Alert.alert('Hata', 'Geçersiz şifre veya yeni şifreler eşleşmiyor.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Change Password</Text>
+      <Text style={styles.label}>Mevcut Şifre</Text>
+      <TextInput
+        secureTextEntry
+        style={styles.input}
+        value={currentPassword}
+        onChangeText={setCurrentPassword}
+      />
+
+      <Text style={styles.label}>Yeni Şifre</Text>
+      <TextInput
+        secureTextEntry
+        style={styles.input}
+        value={newPassword}
+        onChangeText={setNewPassword}
+      />
+
+      <Text style={styles.label}>Yeni Şifre Tekrar</Text>
+      <TextInput
+        secureTextEntry
+        style={styles.input}
+        value={confirmNewPassword}
+        onChangeText={setConfirmNewPassword}
+      />
+
+      <Button title="Şifreyi Değiştir" onPress={handleChangePassword} />
     </View>
   );
 };
@@ -17,9 +54,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
   },
-  title: {
-    fontSize: 24,
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 2, // Kenarları kalın yap
+    borderRadius: 10, // Köşeleri yuvarlak yap
+    marginBottom: 16,
+    paddingHorizontal: 8,
   },
 });
 
-export default ChangePassword;
+export default ChangePasswordScreen;
